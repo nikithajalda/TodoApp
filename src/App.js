c8b8c69570ae04c423206922ad058d1378d0aa09
Nikitha <nikithajalda@gmail.com>
2023-07-13 16:51:37 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React ,{useState,useEffect}from 'react'
 function App() {
+	const [todo,setTodo]= useState();
+	const [todolist,setTodolist]=useState(JSON.parse(localStorage.getItem('todolist')) || []);
+	const [completedlist,setCompletedlist]=useState(JSON.parse(localStorage.getItem('completedlist')) || []);
+
+	useEffect(()=>{
+		localStorage.setItem('todolist',JSON.stringify(todolist))
+		localStorage.setItem('completedlist',JSON.stringify(completedlist));
+	},[todolist,completedlist]);
+
+	const changeHandler = (e) =>{
+		setTodo(e.target.value);
+	}
+	const resetHandler =() =>{
+		setCompletedlist([]);
+		setTodolist([]);
+	}
+	const handleSubmit =() =>{
+		setTodolist([todo,...todolist]);
+		setTodo('');
+	}
+	const handleClick =(id) =>{
+		let temp = todolist, val = todolist[id]
+		temp = temp.filter((element)=> element !== val)
+        console.log(todolist[id]);
+		if(todolist.length > id){
+			setCompletedlist(ref => [val,...ref]);
+			setTodolist(temp)
+		}
+	}
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	<div className='todo'>
+		<input 
+		 type='text'
+		 value={todo}
+		 onChange={changeHandler}
+		 placeholder='Add Todo'
+		/> &nbsp;
+		<button onClick={handleSubmit}>Add Todo</button> &nbsp;
+		<button onClick={resetHandler}>Reset</button> <br/>
+		To Be Completed
+		<div className="todolist">
+			{ 
+			todolist.length? todolist.map((todo,index)=>(
+					<button key={index} onClick={() => handleClick(index)} className='btn'>
+						{todo}	
+					</button>
+				))
+				: null
+			}
+
+		</div>
+		Completed-List 
+		 <div className="todolist">
+			{ 
+			completedlist.length? completedlist.map((todo,index)=>(
+					<button key={index} className='btn'>
+						{todo}	
+					</button>
+				))
+				: null
+
+			}
+
+		 </div>
+	</div>
+  )
 }
 
 export default App;
